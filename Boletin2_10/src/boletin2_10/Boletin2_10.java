52e80cb3e9b732df159ed4d1f85a3f65f5fe297b
abrandarizdominguez <abrandarizdominguez@victoria9.danielcastelao.org>
2017-10-26 09:43:55 +0200

    PRO Boletin 2_10

---
+++ Boletin2_10/src/boletin2_10/Boletin2_10.java
@@ -0,0 +1,31 @@
+
+package boletin2_10;
+
+import java.util.Scanner;
+public class Boletin2_10 {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado=new Scanner(System.in);
+        float soldo_fixo,total_ventas,km,dias_desplazamiento,soldo_bruto,soldo_liquido;
+            System.out.println("Soldo Fixo");
+        soldo_fixo=teclado.nextFloat();
+            System.out.println("Total de Ventas");
+        total_ventas=teclado.nextFloat();
+            System.out.println("Kilometros");
+        km=teclado.nextFloat();
+            System.out.println("Dias de desplazamiento");
+        dias_desplazamiento=teclado.nextFloat();
+        soldo_bruto=(float) (soldo_fixo+(0.05*total_ventas)+2*km+30*dias_desplazamiento);
+        float irpf= 0.18f;
+        float retencion_seguridad_social=36;
+        soldo_liquido=(soldo_bruto-(irpf*soldo_bruto)-retencion_seguridad_social);
+            System.out.println("Sueldo bruto = "+soldo_bruto);
+            System.out.println("Sueldo liquido = "+soldo_liquido);
+        
+        
+        
+    }
+    
+}
