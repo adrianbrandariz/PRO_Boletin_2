b1e3bdf7b6c7520c07c6f571c0f5d176a4a0ac45
abrandarizdominguez <abrandarizdominguez@victoria9.danielcastelao.org>
2017-10-26 09:40:12 +0200

    PRO Boletin 2_5

---
+++ Boletin2_5/src/boletin2_5/Boletin2_5.java
@@ -0,0 +1,25 @@
+
+package boletin2_5;
+
+import java.util.Scanner;
+public class Boletin2_5 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float m = 1852f,millas,metros;
+            System.out.println("Introduce millas:");
+            millas = sc.nextFloat();
+        metros = (millas*m);       
+            System.out.println(millas+" millas son "+metros+" metros.");
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
